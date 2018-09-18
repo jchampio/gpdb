452905c524c7d0c9a2451104d71324775b662ba4
Jacob Champion <pchampion@pivotal.io>
2018-09-18 15:03:52 -0700

    Add GPHOME to the environment whenever gppylib is loaded
    This is an intermediate step; eventually GPHOME shouldn't really be a
    thing except for the very outer layer of the interface.

--- gpMgmt/bin/gppylib/__init__.py
+++ gpMgmt/bin/gppylib/__init__.py
@@ -4,3 +4,12 @@
 from os.path import abspath as _abspath
 __path__[0] = _abspath(__path__[0])
 
+# Set GPHOME based on the library's installation path, if the user hasn't
+# already set it. This relies on gppylib being installed at
+#
+#     ${GPHOME}/bin/gpyylib/
+#
+# TODO: get rid of this hack, and get rid of GPHOME in general.
+import os as _os
+import os.path as _path
+_os.environ['GPHOME'] = _path.dirname(_path.dirname(_path.dirname(_abspath(__file__))))
